8eca5c40ba0182a542f4ea62ad3323e8a79be705
Evgeniy Bobrov <bojeroff@gmail.com>
2022-11-30 16:18:46 +0200

    Homework 1

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// System.Console.WriteLine("Введите Первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите Второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1>num2)
+// {
+//     Console.WriteLine($"Число {num1} больше, чем число {num2}.");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {num2} больше, чем число {num1}.");
+
+// }
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// System.Console.WriteLine("Введите Первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите Второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите Третье число: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// int max = num1;
+// while (count < 2)
+// {
+//     if (num2 > max)
+//     {
+//         max = num2;
+//     }
+//     else if (num3 > max)
+//     {
+//         max = num3;
+//     }
+//     else
+//     { }
+//     count++;
+// }
+// System.Console.WriteLine("Максимальное число " + max);
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// System.Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 2 == 0)
+// {
+//     System.Console.WriteLine($"Число {num} чётное.");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Число {num} нечётное.");
+// }
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// System.Console.WriteLine("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// for (int i = 2; i < N; i += 2)
+// {
+//     System.Console.Write(i + " ");
+    
+// }
+// System.Console.WriteLine("\n" + "Все чётные числа до " + N);
+
+// System.Console.WriteLine("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+// while (count < N)
+// {
+//     System.Console.Write(count + " ");
+//     count += 2;
+    
+// }
+// System.Console.WriteLine("\n" + "Все чётные числа до " + N);
